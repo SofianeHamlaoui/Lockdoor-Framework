918f4d8f91437614efda9cfc2f369f574c4d21bb
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2019-09-06 18:58:05 +0200

    modif_install&Adding_update

--- update.sh
+++ update.sh
@@ -1,18 +1,5 @@
 #!/bin/bash
-
-# Colors
-b='\033[1m'
-u='\033[4m'
-bl='\E[30m'
-r='\E[31m'
-g='\E[32m'
-y='\E[33m'
-bu='\E[34m'
-m='\E[35m'
-c='\E[36m'
-w='\E[37m'
-endc='\E[0m'
-enda='\033[0m'
+#Let's Begin ::
 
 # showing Lockdoor Logo
 function showlogo {
@@ -31,63 +18,35 @@ echo -e  """${r}
 """;
 echo -e "${y} Twitter.com/S0fianeHamlaoui | Github.com/SofianeHamlaoui${endc}"
 }
-
-# Variables : 
-INSTALL_DIR="/opt/sofiane/lockdoor"
-BIN_DIR="/usr/bin/"
-# Functions : 
 function checkroot { # Checking root
   showlogo && sleep 1
   if [[ $(id -u) = 0 ]]; then
     echo -e " Checking For ROOT: ${g}PASSED${endc}"
   else
     echo -e " Checking For ROOT: ${r}FAILED${endc}
- ${y}[!] This Tool Must Run As ROOT [!]${y}"
-    echo -e " ${b}Lockdoor${enda} Will Now Exit"
+ ${y}[!] You Must Run The Update As ROOT [!]${y}"
+    echo -e " ${b}Lockdoor_update${enda} Will Now Exit"
     echo
     sleep 1
     exit
   fi
 }
-######################
-
-echo -e " ${bu}[!] Where do you want to install Lockdoor ? [$INSTALL_DIR]:${endc}" ;
-read option
-if [ ! -z "$option" ]; then
-    INSTALL_DIR="$option"
-    echo "* Directory: $INSTALL_DIR"
-fi
-########################
-
-function dircheck { # Directory Check
-    showlogo && sleep 1
-    if [ -d "$INSTALL_DIR" ]; then
-    echo -e " ${bu}[!] A Directory Lockdoor Was Found.. Do You Want To Update It ? [y/n]:${endc}" ;
-    read rep
-    if [ "$rep" = "y" ]; then
-        rm -R "$INSTALL_DIR"
-        chmod +x update.sh
-        ./update.sh
-        exit
-    fi
-fi
-}
-
-function installing { # Installing Lockdoor
-    showlogo && checkroot && dircheck && sleep 1
-    echo -e "${y}[!] Installing ...${endc}";
+function update {
+    showlogo && checkroot && sleep 1
+    source install.sh
+    rm -rf "$INSTALL_DIR"
+    echo -e "${y}[!] Updating ...${endc}";
     echo "";
     git clone https://github.com/SofianeHamlaoui/Lockdoor-Framework "$INSTALL_DIR" 2>/dev/null;
     if [ -d "$INSTALL_DIR" ] ; then
-    echo -e "${g}[✔] Lockdoor was installed successfully ! Enjoy${endc}" ;
+    echo -e "${g}[✔] Lockdoor was Updated successfully ! Enjoy${endc}" ;
     else
-    echo -e "${r}[✘] Installation Failed !!! [✘]${endc}";
+    echo -e "${r}[✘] Update Failed !!! [✘]${endc}";
     exit
     fi
 }
-
 # calling
-installing
+update
 
 #HAMDOULLILAH <3 
 # End
