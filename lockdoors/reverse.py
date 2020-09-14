1e46709af3a655e49ebbf753c2b6ec1b7479011e
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-14 16:21:13 +0200

    v2.2.4

--- lockdoors/reverse.py
+++ lockdoors/reverse.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from lockdoors import main
+from lockdoors import shrts
 from pathlib import Path
 from datetime import datetime
 from time import sleep
@@ -11,132 +12,132 @@ cwd = os.getcwd()
 null = ""
 ###Cheatsheets
 def revsh():
-    main.clscprilo()
+    shrts.clscprilo()
     print("\033[91mHere is the list of the files :\033[90m")
     print("\033[92m")
-    os.system("     find " + main.getinstalldir() + "/REVERSE/CHEATSHEETS/ -type f")
+    os.system("     find " + shrts.getinstalldir() + "/REVERSE/CHEATSHEETS/ -type f")
     print("\033[90m")
-    main.okrev()
+    shrts.okrev()
 #Tools
 def radar2():
     radar2.title = "Radar 2 : unix-like reverse engineering framework"
     tool_dir = "/REVERSE/Tools/radar2"
-    main.prilogspc()
-    os.system("git clone https://github.com/radare/radare2.git " + main.getinstalldir() + tool_dir + null)
-    main.clscprilo()
+    shrts.prilogspc()
+    os.system("git clone https://github.com/radare/radare2.git " + shrts.getinstalldir() + tool_dir + null)
+    shrts.clscprilo()
     print("\033[92m           Radar2 Downlaoded successfully \033[90m")
-    main.spc()
-    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
-    main.okrev()
+    shrts.spc()
+    print("\033[92m           Check " + shrts.getinstalldir() + tool_dir +" Folder\033[90m")
+    shrts.okrev()
 def virustotal():
     virustotal.title = "VirusTotal tools"
     tool_dir = "/REVERSE/Tools/virustotal"
     if os.path.exists('/usr/local/bin/virustotal'):
-        main.prilogspc()
-        os.system("git clone https://github.com/botherder/virustotal.git " + main.getinstalldir() + tool_dir + null)
-        main.prilogspc()
+        shrts.prilogspc()
+        os.system("git clone https://github.com/botherder/virustotal.git " + shrts.getinstalldir() + tool_dir + null)
+        shrts.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        main.spc()
+        shrts.spc()
         key = input("\033[92mEnter the Virtustoal Api  ? : \033[90m")
         outp = input("\033[92mEnter directory containing files to scan ? : \033[90m")
-        os.system("python2 " + main.getinstalldir() + tool_dir + "/vt.py --key "+key+" " +outp)
-        main.okrev()
+        os.system("python2 " + shrts.getinstalldir() + tool_dir + "/vt.py --key "+key+" " +outp)
+        shrts.okrev()
     else:
-        main.prilogspc()
+        shrts.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        main.spc()
+        shrts.spc()
         print("\033[91mDownloading ...\033[0m")
-        main.spc()
-        os.system("git clone  https://github.com/botherder/virustotal.git  " + main.getinstalldir() + tool_dir + null)
-        main.prilogspc()
+        shrts.spc()
+        os.system("git clone  https://github.com/botherder/virustotal.git  " + shrts.getinstalldir() + tool_dir + null)
+        shrts.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        main.spc()
-        main.prilogspc()
+        shrts.spc()
+        shrts.prilogspc()
         print("\033[91mInstalling ...\033[0m.")
-        main.spc()
+        shrts.spc()
         os.system("""echo "#!/bin/bash" > /usr/local/bin/virustotal""")
         os.system("""echo "#Dev : Sofiane Hamlaoui" >> /usr/local/bin/virustotal""")
-        os.system("echo python2 " + main.getinstalldir() + tool_dir + "/vt.py >> /usr/local/bin/virustotal")
+        os.system("echo python2 " + shrts.getinstalldir() + tool_dir + "/vt.py >> /usr/local/bin/virustotal")
         os.system("chmod +x /usr/local/bin/virustotal")
         print(("You can now use " + "\033[91m" + virustotal.title + "\033[90m" + " from Lockdoor [\033[92m Lockdoor \033[90m ]" ))
-        main.okrev()
+        shrts.okrev()
 def miasm():
     miasm.title = "miasm : Reverse engineering framework"
     tool_dir = "/REVERSE/Tools/miasm"
-    main.prilogspc()
-    os.system("git clone https://github.com/cea-sec/miasm.git " + main.getinstalldir() + tool_dir + null)
-    main.prilogspc()
-    os.system("cd " +main.getinstalldir() + tool_dir + " && python2 setup.py build")
-    os.system("cd " +main.getinstalldir() + tool_dir + " && python2 setup.py install")
-    main.spc()
+    shrts.prilogspc()
+    os.system("git clone https://github.com/cea-sec/miasm.git " + shrts.getinstalldir() + tool_dir + null)
+    shrts.prilogspc()
+    os.system("cd " +shrts.getinstalldir() + tool_dir + " && python2 setup.py build")
+    os.system("cd " +shrts.getinstalldir() + tool_dir + " && python2 setup.py install")
+    shrts.spc()
     print("\033[92m           Miasm Downlaoded successfully \033[90m")
-    main.spc()
-    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
-    main.okrev()
+    shrts.spc()
+    print("\033[92m           Check " + shrts.getinstalldir() + tool_dir +" Folder\033[90m")
+    shrts.okrev()
 def mirror():
     mirror.title = "mirror : reverses the bytes of a file"
     tool_dir = "/REVERSE/Tools/mirror"
-    main.prilogspc()
-    os.system("git clone https://github.com/guelfoweb/mirror.git " + main.getinstalldir() + tool_dir + null)
-    main.clr()
-    main.prilogspc()
+    shrts.prilogspc()
+    os.system("git clone https://github.com/guelfoweb/mirror.git " + shrts.getinstalldir() + tool_dir + null)
+    shrts.clr()
+    shrts.prilogspc()
     print("\033[92m           Mirror Downlaoded successfully \033[90m")
-    main.spc()
-    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
-    main.okrev()
+    shrts.spc()
+    print("\033[92m           Check " + shrts.getinstalldir() + tool_dir +" Folder\033[90m")
+    shrts.okrev()
 def Dnspy():
     Dnspy.title = "Dnspy : reverses the bytes of a file"
     tool_dir = "/REVERSE/Tools/Dnspy"
-    main.prilogspc()
-    os.system("git clone https://github.com/0xd4d/dnSpy.git " + main.getinstalldir() + tool_dir + null)
-    main.clr()
-    main.prilogspc()
+    shrts.prilogspc()
+    os.system("git clone https://github.com/0xd4d/dnSpy.git " + shrts.getinstalldir() + tool_dir + null)
+    shrts.clr()
+    shrts.prilogspc()
     print("\033[92m           Dnspy Downlaoded successfully \033[90m")
-    main.spc()
-    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
-    main.okrev()
+    shrts.spc()
+    print("\033[92m           Check " + shrts.getinstalldir() + tool_dir +" Folder\033[90m")
+    shrts.okrev()
 def angrio():
     angrio.title = "angrio : a python framework for analyzing binaries"
     tool_dir = "/REVERSE/Tools/angrio"
-    main.prilogspc()
+    shrts.prilogspc()
     print("\033[92m           Installing \033[90m")
-    main.spc()
+    shrts.spc()
     os.system("pip install angr ")
-    main.clr()
-    main.prilogspc()
+    shrts.clr()
+    shrts.prilogspc()
     print("\033[92m           Dnspy Downlaoded successfully \033[90m")
-    main.spc()
+    shrts.spc()
     print("\033[92m           Check Angr.io docs to learn more about the tool \033[90m")
     print("\033[92m           https://github.com/angr/angr-doc \033[90m")
-    main.okrev()
+    shrts.okrev()
 def dllrunner():
     dllrunner.title = "Dllrunner : a smart DLL execution script for malware analysis"
     tool_dir = "/REVERSE/Tools/dllrunner"
-    main.prilogspc()
-    os.system("git clone https://github.com/Neo23x0/DLLRunner " + main.getinstalldir() + tool_dir + null)
-    main.clr()
-    main.prilogspc()
+    shrts.prilogspc()
+    os.system("git clone https://github.com/Neo23x0/DLLRunner " + shrts.getinstalldir() + tool_dir + null)
+    shrts.clr()
+    shrts.prilogspc()
     print("\033[92m           Dllrunner Downlaoded successfully \033[90m")
-    main.spc()
-    print("\033[92m           Check "+ main.getinstalldir() + tool_dir + " Folder\033[90m")
-    main.okrev()
+    shrts.spc()
+    print("\033[92m           Check "+ shrts.getinstalldir() + tool_dir + " Folder\033[90m")
+    shrts.okrev()
 def yara():
     yara.title = "YARA :  a tool to identify and classify malwares "
     tool_dir = "/REVERSE/Tools/yara"
-    main.prilogspc()
+    shrts.prilogspc()
     print("\033[92m           Installing \033[90m")
-    main.spc()
+    shrts.spc()
     os.system("pip install yara-python")
-    main.clr()
-    main.prilogspc()
+    shrts.clr()
+    shrts.prilogspc()
     print("\033[92m           YARA Downlaoded successfully \033[90m")
-    main.spc()
+    shrts.spc()
     print("\033[92m           Check YARA Docs to learn more about the tool\033[90m")
     print("\033[92m           https://yara.readthedocs.io/en/latest/\033[90m")
-    main.okrev()
+    shrts.okrev()
 #Menu
 def menu():
-    main.clscprilo()
+    shrts.clscprilo()
     print("""\033[94m
     [ REVERSE ENGINEERING  ]
 
@@ -181,11 +182,11 @@ def menu():
     elif choice == "b":
       main.menu()
     elif choice == "q":
-        main.prilogspc()
+        shrts.prilogspc()
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print("                 \033[91m-[!]- LOCKDOOR IS EXITING -[!]-\033[0m")
-        main.spc()
+        shrts.spc()
         print("                 \033[91m-[!]- EXITING AT " + dt_string + " -[!]-\033[0m")
         sys.exit()
     elif choice == "":
