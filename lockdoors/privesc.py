bb3063d09e53cde5bcad6e61533c994f66fa5f32
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-14 16:21:06 +0200

    v2.2.4

--- lockdoors/privesc.py
+++ lockdoors/privesc.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from lockdoors import main
+from lockdoors import shrts
 from pathlib import Path
 from datetime import datetime
 from time import sleep
@@ -11,29 +12,29 @@ cwd = os.getcwd()
 null = ""
 ###Cheatsheets
 def privesh():
-    main.clscprilo()
+    shrts.clscprilo()
     print("\033[91mHere is the list of the files :\033[90m")
     print("\033[92m")
-    os.system("     find " + main.getinstalldir() + "/PrivEsc/CHEATSHEETS -type f")
+    os.system("     find " + shrts.getinstalldir() + "/PrivEsc/CHEATSHEETS -type f")
     print("\033[90m")
-    main.okpr()
+    shrts.okpr()
 #Tools
 def priivesc():
     priivesc.title=("A collection of Windows, Linux and MySQL privilege escalation scripts and exploits")
-    main.printlogo()
+    shrts.printlogo()
     print("\033[92m" + priivesc.title + "\033[90m")
     print()
     print("\033[91mDownloading ...\033[0m")
-    main.spc()
-    os.system("git clone https://github.com/1N3/PrivEsc.git " + main.getinstalldir() + "/PrivEsc/Scripts")
-    main.spc()
+    shrts.spc()
+    os.system("git clone https://github.com/1N3/PrivEsc.git " + shrts.getinstalldir() + "/PrivEsc/Scripts")
+    shrts.spc()
     print("\033[91m" + """\033[91mThe collection of Windows, Linux and MySQL privilege
     escalation scripts and exploits is downloaded successfully , you can check
-    it here : """ + "\033[90m" + main.getinstalldir() + "/PrivEsc/Scripts")
-    main.okpr()
+    it here : """ + "\033[90m" + shrts.getinstalldir() + "/PrivEsc/Scripts")
+    shrts.okpr()
 #Menu
 def menu():
-    main.clscprilo()
+    shrts.clscprilo()
     print("""\033[94m
        [ PRIVILEGE ESCALATION ]
 
@@ -58,11 +59,11 @@ def menu():
     elif choice == "b":
       main.menu()
     elif choice == "q":
-        main.prilogspc()
+        shrts.prilogspc()
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print("                 \033[91m-[!]- LOCKDOOR IS EXITING -[!]-\033[0m")
-        main.spc()
+        shrts.spc()
         print("                 \033[91m-[!]- EXITING AT " + dt_string + " -[!]-\033[0m")
         sys.exit()
     elif choice == "":
