3e26960a5571ae7442ce0d1ca0b4b41247be2367
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-14 15:51:43 +0200

    v2.2.4

--- lockdoors/reverse.py
+++ lockdoors/reverse.py
@@ -4,192 +4,140 @@ from lockdoors import main
 from pathlib import Path
 from datetime import datetime
 from time import sleep
-def printlogo():
-    print("""
-\033[94m            ..',,,'..           \033[0m
-\033[94m         .',;;;;;;;;,'.         \033[0m
-\033[94m      ..,;;;;;;;;;;;;;;,..      \033[0m
-\033[94m     .,;;;,'..'''''.',;;;,.     \033[0m
-\033[94m     .;;;;.  ..   .. .;;;;'     \033[0m\033[91m (                                         \033[0m
-\033[94m     .,;;;.  ...     .;;;;.     \033[0m\033[91m )\ )               )  (                   \033[0m
-\033[94m      ..,;,.  ...   .,;,..      \033[0m\033[91m (()/(            ( /(  )\ )           (   \033[0m
-\033[94m        .';;'.    .',;'.        \033[0m\033[91m /(_))  (    (   )\())(()/(  (    (   )(   \033[0m
-\033[94m    ..',,;;;;;,,,,;;;;;,,'..    \033[0m\033[91m (_))    )\   )\ ((_)\  ((_)) )\   )\ (()\ \033[0m
-\033[94m  .','.....................''.  \033[0m\033[91m | |    ((_) ((_)| |(_) _| | ((_) ((_) ((_)\033[0m
-\033[94m .',..',,,,,,,,,,,,,,,,,,,..,,. \033[0m\033[91m | |__ / _ \/ _| | / // _` |/ _ \/ _ \| '_|\033[0m
-\033[94m .;,..,;;;;;;'....';;;;;;;..,;. \033[0m\033[91m |____|\___/\__| |_\_\\__,_|\___/\___/|_|  \033[0m
-\033[94m ';;..,;;;;;,..,,..';;;;;,..,;' \033[0m\033[92m           Sofiane Hamlaoui | 2019         \033[0m
-\033[94m.';;..,;;;;,. .... .,;;;;,..;;,.\033[0m\033[92m Lockdoor : A Penetration Testing framework\033[0m
-\033[94m ';;..,;;;;'  ....  .;;;;,..;;,. \033[0m
-\033[94m .,;'.';;;;'.  ..  .';;;;,.';,.  \033[0m
-\033[94m   ....;;;;;,'''''',;;;;;'...    \033[0m
-\033[94m       ..................\033[0m""")
 #VAR
 config = str(Path.home()) + "/.config/lockdoor/"
 yes = set(['yes', 'y', 'ye', 'Y'])
 no = set(['no', 'n', 'nop', 'N'])
 cwd = os.getcwd()
 null = ""
-#Config
-f = open(config + 'lockdoor.conf')
-contents = f.read().rstrip('\n')
-f.close()
-installdirc = contents.replace('Location:', '')
-##########SHRT
-def oktocont():
-    ans = input("\033[0;36mPress Enter to Continue...\033[0m")
-def clr():
-    os.system('clear')
-def spc():
-    print("")
-def prilogspc():
-    printlogo()
-    spc()
-def clscprilo():
-    clr()
-    printlogo()
-def popp():
-    spc()
-    oktocont()
-    printlogo()
-    spc()
-def okrev():
-    spc()
-    oktocont()
-    menu()
-def pop():
-    spc()
-    oktocont()
-    spc()
-############
 ###Cheatsheets
 def revsh():
-    clscprilo()
+    main.clscprilo()
     print("\033[91mHere is the list of the files :\033[90m")
     print("\033[92m")
-    os.system("     find " + installdirc + "/REVERSE/CHEATSHEETS/ -type f")
+    os.system("     find " + main.getinstalldir() + "/REVERSE/CHEATSHEETS/ -type f")
     print("\033[90m")
-    okrev()
+    main.okrev()
 #Tools
 def radar2():
     radar2.title = "Radar 2 : unix-like reverse engineering framework"
     tool_dir = "/REVERSE/Tools/radar2"
-    prilogspc()
-    os.system("git clone https://github.com/radare/radare2.git " + installdirc + tool_dir + null)
-    clscprilo()
+    main.prilogspc()
+    os.system("git clone https://github.com/radare/radare2.git " + main.getinstalldir() + tool_dir + null)
+    main.clscprilo()
     print("\033[92m           Radar2 Downlaoded successfully \033[90m")
-    spc()
-    print("\033[92m           Check " + installdirc + tool_dir +" Folder\033[90m")
-    okrev()
+    main.spc()
+    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
+    main.okrev()
 def virustotal():
     virustotal.title = "VirusTotal tools"
     tool_dir = "/REVERSE/Tools/virustotal"
     if os.path.exists('/usr/local/bin/virustotal'):
-        prilogspc()
-        os.system("git clone https://github.com/botherder/virustotal.git " + installdirc + tool_dir + null)
-        prilogspc()
+        main.prilogspc()
+        os.system("git clone https://github.com/botherder/virustotal.git " + main.getinstalldir() + tool_dir + null)
+        main.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        spc()
+        main.spc()
         key = input("\033[92mEnter the Virtustoal Api  ? : \033[90m")
         outp = input("\033[92mEnter directory containing files to scan ? : \033[90m")
-        os.system("python2 " + installdirc + tool_dir + "/vt.py --key "+key+" " +outp)
-        okrev()
+        os.system("python2 " + main.getinstalldir() + tool_dir + "/vt.py --key "+key+" " +outp)
+        main.okrev()
     else:
-        prilogspc()
+        main.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        spc()
+        main.spc()
         print("\033[91mDownloading ...\033[0m")
-        spc()
-        os.system("git clone  https://github.com/botherder/virustotal.git  " + installdirc + tool_dir + null)
-        prilogspc()
+        main.spc()
+        os.system("git clone  https://github.com/botherder/virustotal.git  " + main.getinstalldir() + tool_dir + null)
+        main.prilogspc()
         print("\033[92m           " + virustotal.title + "\033[90m")
-        spc()
-        prilogspc()
+        main.spc()
+        main.prilogspc()
         print("\033[91mInstalling ...\033[0m.")
-        spc()
+        main.spc()
         os.system("""echo "#!/bin/bash" > /usr/local/bin/virustotal""")
         os.system("""echo "#Dev : Sofiane Hamlaoui" >> /usr/local/bin/virustotal""")
-        os.system("echo python2 " + installdirc + tool_dir + "/vt.py >> /usr/local/bin/virustotal")
+        os.system("echo python2 " + main.getinstalldir() + tool_dir + "/vt.py >> /usr/local/bin/virustotal")
         os.system("chmod +x /usr/local/bin/virustotal")
         print(("You can now use " + "\033[91m" + virustotal.title + "\033[90m" + " from Lockdoor [\033[92m Lockdoor \033[90m ]" ))
-        okrev()
+        main.okrev()
 def miasm():
     miasm.title = "miasm : Reverse engineering framework"
     tool_dir = "/REVERSE/Tools/miasm"
-    prilogspc()
-    os.system("git clone https://github.com/cea-sec/miasm.git " + installdirc + tool_dir + null)
-    prilogspc()
-    os.system("cd " +installdirc + tool_dir + " && python2 setup.py build")
-    os.system("cd " +installdirc + tool_dir + " && python2 setup.py install")
-    spc()
+    main.prilogspc()
+    os.system("git clone https://github.com/cea-sec/miasm.git " + main.getinstalldir() + tool_dir + null)
+    main.prilogspc()
+    os.system("cd " +main.getinstalldir() + tool_dir + " && python2 setup.py build")
+    os.system("cd " +main.getinstalldir() + tool_dir + " && python2 setup.py install")
+    main.spc()
     print("\033[92m           Miasm Downlaoded successfully \033[90m")
-    spc()
-    print("\033[92m           Check " + installdirc + tool_dir +" Folder\033[90m")
-    okrev()
+    main.spc()
+    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
+    main.okrev()
 def mirror():
     mirror.title = "mirror : reverses the bytes of a file"
     tool_dir = "/REVERSE/Tools/mirror"
-    prilogspc()
-    os.system("git clone https://github.com/guelfoweb/mirror.git " + installdirc + tool_dir + null)
-    clr()
-    prilogspc()
+    main.prilogspc()
+    os.system("git clone https://github.com/guelfoweb/mirror.git " + main.getinstalldir() + tool_dir + null)
+    main.clr()
+    main.prilogspc()
     print("\033[92m           Mirror Downlaoded successfully \033[90m")
-    spc()
-    print("\033[92m           Check " + installdirc + tool_dir +" Folder\033[90m")
-    okrev()
+    main.spc()
+    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
+    main.okrev()
 def Dnspy():
     Dnspy.title = "Dnspy : reverses the bytes of a file"
     tool_dir = "/REVERSE/Tools/Dnspy"
-    prilogspc()
-    os.system("git clone https://github.com/0xd4d/dnSpy.git " + installdirc + tool_dir + null)
-    clr()
-    prilogspc()
+    main.prilogspc()
+    os.system("git clone https://github.com/0xd4d/dnSpy.git " + main.getinstalldir() + tool_dir + null)
+    main.clr()
+    main.prilogspc()
     print("\033[92m           Dnspy Downlaoded successfully \033[90m")
-    spc()
-    print("\033[92m           Check " + installdirc + tool_dir +" Folder\033[90m")
-    okrev()
+    main.spc()
+    print("\033[92m           Check " + main.getinstalldir() + tool_dir +" Folder\033[90m")
+    main.okrev()
 def angrio():
     angrio.title = "angrio : a python framework for analyzing binaries"
     tool_dir = "/REVERSE/Tools/angrio"
-    prilogspc()
+    main.prilogspc()
     print("\033[92m           Installing \033[90m")
-    spc()
+    main.spc()
     os.system("pip install angr ")
-    clr()
-    prilogspc()
+    main.clr()
+    main.prilogspc()
     print("\033[92m           Dnspy Downlaoded successfully \033[90m")
-    spc()
+    main.spc()
     print("\033[92m           Check Angr.io docs to learn more about the tool \033[90m")
     print("\033[92m           https://github.com/angr/angr-doc \033[90m")
-    okrev()
+    main.okrev()
 def dllrunner():
     dllrunner.title = "Dllrunner : a smart DLL execution script for malware analysis"
     tool_dir = "/REVERSE/Tools/dllrunner"
-    prilogspc()
-    os.system("git clone https://github.com/Neo23x0/DLLRunner " + installdirc + tool_dir + null)
-    clr()
-    prilogspc()
+    main.prilogspc()
+    os.system("git clone https://github.com/Neo23x0/DLLRunner " + main.getinstalldir() + tool_dir + null)
+    main.clr()
+    main.prilogspc()
     print("\033[92m           Dllrunner Downlaoded successfully \033[90m")
-    spc()
-    print("\033[92m           Check "+ installdirc + tool_dir + " Folder\033[90m")
-    okrev()
+    main.spc()
+    print("\033[92m           Check "+ main.getinstalldir() + tool_dir + " Folder\033[90m")
+    main.okrev()
 def yara():
     yara.title = "YARA :  a tool to identify and classify malwares "
     tool_dir = "/REVERSE/Tools/yara"
-    prilogspc()
+    main.prilogspc()
     print("\033[92m           Installing \033[90m")
-    spc()
+    main.spc()
     os.system("pip install yara-python")
-    clr()
-    prilogspc()
+    main.clr()
+    main.prilogspc()
     print("\033[92m           YARA Downlaoded successfully \033[90m")
-    spc()
+    main.spc()
     print("\033[92m           Check YARA Docs to learn more about the tool\033[90m")
     print("\033[92m           https://yara.readthedocs.io/en/latest/\033[90m")
-    okrev()
+    main.okrev()
 #Menu
 def menu():
-    clscprilo()
+    main.clscprilo()
     print("""\033[94m
     [ REVERSE ENGINEERING  ]
 
@@ -234,11 +182,11 @@ def menu():
     elif choice == "b":
       main.menu()
     elif choice == "q":
-        prilogspc()
+        main.prilogspc()
         now = datetime.now()
         dt_string = now.strftime("%d/%m/%Y %H:%M:%S")
         print("                 \033[91m-[!]- LOCKDOOR IS EXITING -[!]-\033[0m")
-        spc()
+        main.spc()
         print("                 \033[91m-[!]- EXITING AT " + dt_string + " -[!]-\033[0m")
         sys.exit()
     elif choice == "":
