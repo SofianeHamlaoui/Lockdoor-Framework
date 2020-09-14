1b48a5d9f6378a60b2dc82eb1e211f64d4c53384
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-14 16:21:29 +0200

    v2.2.4

--- lockdoors/update.py
+++ lockdoors/update.py
@@ -1,22 +1,23 @@
 import os
 from pathlib import Path
+from lockdoors import shrts
 from lockdoors import main
 yes = set(['yes', 'y', 'ye', 'Y'])
 no = set(['no', 'n', 'nop', 'N'])
 
 def lockdoor():
-    main.clr()
-    main.printlogo()
-    main.spc()
+    shrts.clr()
+    shrts.printlogo()
+    shrts.spc()
     choiceupdate = input("        \033[92m[Confirmation]\033[90m\033[94m You want to update Lockdoor and all tools ? (Y / N) : \033[90m")
     if not choiceupdate in no:
         print("\033[92mUpdating Lockdoor...\033[90m")
-        main.spc()
+        shrts.spc()
         os.system('pip install --upgrade lockdoor')
-        main.spc()
-        main.oktocont()
+        shrts.spc()
+        shrts.oktocont()
         print("\033[92mUpdating The Tools...\033[90m")
-        main.spc()
-        os.system('sudo find ' + main.getinstalldir() + ' -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull origin master" \;')
-        main.clr()
+        shrts.spc()
+        os.system('sudo find ' + shrts.getinstalldir() + ' -type d -name .git -exec sh -c "cd \"{}\"/../ && pwd && git pull origin master" \;')
+        shrts.clr()
         main.menu()
