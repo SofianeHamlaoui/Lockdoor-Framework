07ea698b21c293d9d010c8e848e271046391ca66
Sofiane Hamlaoui <s0fianehaml40u1@gmail.com>
2020-09-14 15:56:36 +0200

    v2.2.4

--- lockdoors/psafrt.py
+++ lockdoors/psafrt.py
@@ -5,34 +5,17 @@ from pathlib import Path
 from datetime import datetime
 from time import sleep
 #VAR
-config = str(Path.home()) + "/.config/lockdoor/"
 yes = set(['yes', 'y', 'ye', 'Y'])
 no = set(['no', 'n', 'nop', 'N'])
 cwd = os.getcwd()
-null = ""
-#Config
-f = open(config + 'lockdoor.conf')
-contents = f.read().rstrip('\n')
-f.close()
-installdirc = contents.replace('Location:', '')
-##########SHRT
-def oktocont():
-    ans = input("\033[0;36mPress Enter to Continue...\033[0m")
-def clr():
-    os.system('clear')
-def spc():
-    print("")
-def clscprilo():
-    clr()
-    main.printlogo()   
-############
+null = "" 
 ###Fonc
 def psafrt():
-    clscprilo()
+    main.clscprilo()
     print("\033[91mHere is the list of the files :\033[90m")
     print("\033[92m")
-    os.system("     find " + installdirc + "/REPORT/TEMPLATES/ -type f")
+    os.system("     find " + main.getinstalldir() + "/REPORT/TEMPLATES/ -type f")
     print("\033[90m")
-    spc()
-    oktocont()
+    main.spc()
+    main.oktocont()
     main.menu()
