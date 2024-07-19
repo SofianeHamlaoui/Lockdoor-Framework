20fb9a88628f44796cdb92c4689f9d686d929c3c
keepittechie <joshlacy@gmail.com>
2024-07-18 20:06:07 -0700

    Updated list of packages for apt to remove errors while installing. Some names have changed in the repositories. "python" package is not available; python3 fixes this. netcat isn't the correct name, causes error. Changed to netcat-traditional.

--- install.sh
+++ install.sh
@@ -60,7 +60,7 @@ function chkapt {
     if [ "$?" -eq "0" ]; then
         echo -e "   ${g} [-] Installing the Packages${endc}"
         echo ""
-        apt-get install -y golang-go python python3-pip python3 python3-requests python3-pip gcc ruby php git wget bc curl netcat subversion openjdk-11-jre make automake gcc gzip rsync wget
+        apt-get install -y golang-go python3 python3-pip python3-requests python3-pip gcc ruby php git wget bc curl netcat-traditional subversion openjdk-11-jre make automake gcc gzip rsync wget
         gem install bundler:1.17.2
     else
         echo -e "   ${g} [-] Skipping apt${endc}"
